baaf0903889f251e6b1112eace34beece01889e3
ricky <125274483+patrickNwafo@users.noreply.github.com>
2024-03-30 17:02:41 +0100

    deploymentF

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
         if (form.prompt) {
             try {
                 setGeneratingImg(true);
-                const response = await fetch('http://localhost:8080/api/v1/dalle', {
+                const response = await fetch('https://dall-e-0194.onrender.com/api/v1/dalle', {
                     method: "POST",
                     headers: {
                         'Content-Type': 'application/json',
@@ -46,7 +46,7 @@ const CreatePost = () => {
             setLoading(true);
 
             try {
-                const response = await fetch('http://localhost:8080/api/v1/post', {
+                const response = await fetch('https://dall-e-0194.onrender.com/api/v1/post', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
