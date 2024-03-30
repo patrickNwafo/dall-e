ff8d7a5d288b9d2a92cd700fe64945bdf02c5473
ricky <125274483+patrickNwafo@users.noreply.github.com>
2024-03-30 16:18:02 +0100

    deployment

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/post', postRoutes);
